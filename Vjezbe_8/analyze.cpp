79e0f663fc50b54afcad21c385e02d86ce46eee2
sboic <sboic@pmfst.hr>
2023-12-10 16:51:33 +0100

    vjezbe_8

---
+++ Vjezbe_8/analyze.cpp
@@ -0,0 +1,16 @@
+#include "Analyzer.h"
+
+int main(void){
+
+    Analyzer *analyzer = new Analyzer();
+    analyzer -> PlotHistogram("/home/public/data/ggH125/ZZ4lAnalysis.root");
+    analyzer -> PlotHistogram("/home/public/data/qqZZ/ZZ4lAnalysis.root");
+    analyzer -> PlotMass();
+    analyzer -> PlotDkin();
+
+    analyzer -> FitHiggsMass();
+    analyzer -> FitHiggsML();
+
+    delete analyzer;
+    return 0;
+}
